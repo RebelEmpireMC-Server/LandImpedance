4c7b276549e22c30f26f8548a689ed82f2d605f3
Axe2760 <null>
2014-01-28 08:04:13 -0600

    Setup project

---
+++ src/com/rebelempiremc/landimpedance/LandImpedance.java
@@ -0,0 +1,9 @@
+package com.rebelempiremc.landimpedance;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LandImpedance extends JavaPlugin {
+    public void onEnable(){
+        
+    }
+}
